f894fa23a48751b7bbff9a165542ceda3ce9f606
SnowyPython <135994019+SnowyPython@users.noreply.github.com>
2024-10-03 12:32:37 +0500

    Stage one

---
+++ src/Creature.java
@@ -0,0 +1,6 @@
+public abstract class Creature extends Entity {
+    private int speed;
+    private int hp;
+
+    public abstract void makeMove();
+}
---
+++ src/Predator.java
@@ -0,0 +1,8 @@
+public class Predator extends Creature {
+    private int power;
+
+    @Override
+    public void makeMove() {
+
+    }
+}
